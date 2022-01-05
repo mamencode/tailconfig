d7d58d424e7e88961d233e253aa6f5843d752428
Mamen code <72928405+mamencode@users.noreply.github.com>
2022-01-05 19:54:16 +0300

    Update App.js
    Updating app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+
+  return(
+    <div className="bg-gray-100 max-w-sm mx-auto min-h-screen">
+
+) 
 }
 
 export default App;
